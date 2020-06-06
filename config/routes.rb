854be6af947da27a581cb81e562d7937465d204e
Kazuto <1234567u1234567u@gmail.com>
2020-06-06 20:59:12 +0000

    make Users controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/terms_of_use'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
