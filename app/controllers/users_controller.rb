17533191c388d6e286d934ddb978064862c542b6
Kazuto <1234567u1234567u@gmail.com>
2020-06-06 22:38:32 +0000

    add create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def show
   end
   
   def edit
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:full_name, :user_name, :password, :password_confirmation)
   end
 end
