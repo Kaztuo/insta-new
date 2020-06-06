3292735463bd85b8d6cb72d9c4be251595c47d13
Kazuto <1234567u1234567u@gmail.com>
2020-06-06 21:15:21 +0000

    add full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_titel = '')
+      base_titel = 'Instagram App'
+      if page_titel.any?
+        page_titel + " | " + base_titel
+      else
+        base_titel
+      end
+    end
 end
