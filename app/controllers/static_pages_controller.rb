de12ee27d9c7c324c16ba7b9f796841da43cbaa7
Kazuto <1234567u1234567u@gmail.com>
2020-06-06 12:55:21 +0000

    make StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def terms_of_use
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/terms_of_use'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
