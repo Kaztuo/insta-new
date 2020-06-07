a046a5b3919a5100965bf4e7b0809084600c4b54
Kazuto <1234567u1234567u@gmail.com>
2020-06-07 22:59:24 +0000

    success redirect_to @user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user = User.find(params[:id])
   end
   
   def edit
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render 'show'
+      redirect_to @user  
     else
       render 'new'
     end
