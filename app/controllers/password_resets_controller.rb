93d75f915f3c97e29a4468ca32a848641767010a
Kazuto <1234567u1234567u@gmail.com>
2020-06-10 00:05:48 +0000

    create password edit page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,    only: [:edit, :update]
+  
   def new
   end
 
@@ -8,6 +10,25 @@ class PasswordResetsController < ApplicationController
   def create
   end
   
-  def update
-  end 
+  
+def update
+    if @user.authenticate("#{params[:password_exist]}") && params[:password] == params[:password_confirmation]
+      @user.update(password_params)
+      flash[:success] = "パスワードが更新されました."
+      redirect_to @user
+    else
+      flash[:danger] = "「現在のパスワード」が間違っているか、「新しいパスワード」と「新しいパスワードの確認入力欄」が一致しません。"
+      render 'edit'
+    end
+end
+  
+  private
+  
+  def get_user
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def password_params
+    params.permit(:password, :password_confirmation)
+  end
 end
