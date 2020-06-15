ccf750fe6dbe56b618cf560672a6a2f9543da314
Kazuto <1234567u1234567u@gmail.com>
2020-06-15 01:08:15 +0000

    finish photo-page

--- test/integration/photo_page_test.rb
+++ test/integration/photo_page_test.rb
@@ -8,8 +8,14 @@ class PhotoPageTest < ActionDispatch::IntegrationTest
     get new_photo_path
     assert_select 'input[type=file]'
     assert_select 'input[type=submit]'
+    
     assert_no_difference 'Photo.count' do
-      post photos_path, params: { photo: { image: "", id: "" } }
+      post photos_path, params: { photo: { user_id: "" } }
+    end
+    
+    test_image = fixture_file_upload('test/fixtures/test_photo.jpg', 'image/jpg')
+    assert_difference 'Photo.count', 1 do
+      post photos_path, params: { photo: { image: test_image } }
     end
   end
 end
