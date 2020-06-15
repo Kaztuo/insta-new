d1ef6e853af6a26a64ebca28e0fc2c614cb0c289
Kazuto <1234567u1234567u@gmail.com>
2020-06-15 02:41:34 +0000

    prepare relationships functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,41 @@
 class User < ApplicationRecord
   has_many :photos, dependent: :destroy
+  has_many :active_relationships,  class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                    dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                    dependent: :destroy
+                                   
   has_secure_password
-  
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships #, source: :follower 
+                                                       #Railsが「followers」を単数形にして
+                                                       #自動的に外部キーfollower_idを探してくれる
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  # 現在のユーザーがフォローされてたらtrueを返す  
+  def followed?(other_user)
+    followers.include?(other_user)
+  end
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should follow and unfollow a user" do
+    user1 = users(:example_user_one)
+    user2  = users(:example_user_two)
+    assert_not user1.following?(user2)
+    user1.follow(user2)
+    assert user1.following?(user2)
+    assert user2.followed?(user1)    
+    user1.unfollow(user2)
+    assert_not user1.following?(user2)
+  end
 end
