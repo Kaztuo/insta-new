59c39f26d2f39096421d06e61d34ed491e077327
Kazuto <1234567u1234567u@gmail.com>
2020-06-30 02:11:42 +0000

    arguments error user create

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.facebook_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+  
+  def failure
+    redirect_to root_path
+  end
+end
