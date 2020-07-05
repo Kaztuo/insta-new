54feaa4ee97ff87cf2437d855cfd9f62324bc0a3
Kazuto <1234567u1234567u@gmail.com>
2020-07-05 10:11:30 +0000

    db migrate fix index to notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_020512) do
+ActiveRecord::Schema.define(version: 2020_07_05_100710) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
