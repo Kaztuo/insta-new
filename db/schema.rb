8dc1523e94a43802e00a921252ead04e3eeffc47
Kazuto <1234567u1234567u@gmail.com>
2020-07-05 20:48:08 +0000

    change migration notification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_113707) do
+ActiveRecord::Schema.define(version: 2020_06_30_020512) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
