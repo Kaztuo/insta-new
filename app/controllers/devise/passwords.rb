1428ec5cf4b5ee93427b6981e85bf8ae1f3c423a
Kazuto <1234567u1234567u@gmail.com>
2020-07-09 00:43:55 +0000

    finish

--- app/controllers/devise/passwords.rb
+++ app/controllers/devise/passwords.rb
@@ -1,6 +1,5 @@
 class Devise::PasswordsController < DeviseController
   prepend_before_action :require_no_authentication
-  # Render the #edit only if coming from a reset password email link
   append_before_action :assert_reset_token_passed, only: :edit 
   
   def edit
