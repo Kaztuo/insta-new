a3d1fbdd86e5c47cad1d2e3818b33a2f7b98b3a4
Kazuto <1234567u1234567u@gmail.com>
2020-07-12 20:46:44 +0000

    rubyversionをgitignoreから外すとpushするファイルが作成された

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  # before_action :set_search
+
+  # def set_search
+  #   @q = Comment.ransack(params[:q])
+  #   @search_comments = @q.result(distinct: true)
+  # end
+
+  private
+
+  # ユーザーのログインを確認する
+  # def logged_in_user
+  #   unless user_signed_in? #logged_in?から変更
+  #     store_location
+  #     flash[:danger] = "ログインしてください"
+  #     redirect_to new_user_session_url #login_url
+  #   end
+  # end
+end
